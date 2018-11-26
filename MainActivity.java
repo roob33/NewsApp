fb5acc747a8faefcc424cacb4bf63e70e23ede95
roob33 <45367434+roob33@users.noreply.github.com>
2018-11-26 23:30:12 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.android.newsapp;
+
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
+
+    private static final String NEWS_API_URL = "https://content.guardianapis.com/search?api-key=19a9b57e-232d-42cf-a100-91b13f6424c4";
+    private static final int NEWS_LOADER_ID = 1;
+    NewsAdapter mAdapter;
+    TextView mEmptyStateTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView newsListView = findViewById(R.id.list);
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+        newsListView.setAdapter(mAdapter);
+
+
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                News currentNews = mAdapter.getItem(position);
+
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                startActivity(websiteIntent);
+            }
+        });
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String selectSection = sharedPreferences.getString(
+                getString(R.string.settings_section_by_key),
+                getString(R.string.settings_section_by_default));
+        String pageNumber = sharedPreferences.getString(
+                getString(R.string.settings_page_key),
+                getString(R.string.settings_page_default));
+
+
+        Uri baseUri = Uri.parse(NEWS_API_URL);
+        Uri.Builder builder = baseUri.buildUpon();
+
+        if (!selectSection.equals("all")) {
+            builder.appendQueryParameter("section", selectSection);
+        }
+        builder.appendQueryParameter("show-fields", "byline");
+        builder.appendQueryParameter("page-size", pageNumber);
+        return new NewsLoader(this, builder.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        mEmptyStateTextView.setText(R.string.no_internet_connection);
+        mAdapter.clear();
+
+        if (data != null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
